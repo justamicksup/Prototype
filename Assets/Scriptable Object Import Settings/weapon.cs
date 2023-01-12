f5a26e5a27c8eebea213af194038e434ffbbe17c
Amber Smith <JRSmith9@student.fullsail.edu>
2023-01-11 20:31:21 -0700

    Made more working ranged weapons.

--- Assets/Scriptable Object Import Settings/weapon.cs
+++ Assets/Scriptable Object Import Settings/weapon.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class weapon : MonoBehaviour
 {
-    [Range(0.1f, 5)] [SerializeField] public float shootRate;
+    [Range(0.1f, 10)] [SerializeField] public float shootRate;
     [Range(1, 100)] [SerializeField] public int shootDist;
     [Range(1, 10)] [SerializeField] public int shootDamage;
     [Range(1, 10)] [SerializeField] public float shootForce;
     [Range(1, 100)] [SerializeField] public int ammoCapacity;
     [Range(1, 100)] [SerializeField] public int ammoRemaining;
-    [Range(0.1f, 5)] [SerializeField] public float reloadTime;
+    [Range(0.1f, 10)] [SerializeField] public float reloadTime;
     [SerializeField] public Mesh viewModel;
 
     void Start()
