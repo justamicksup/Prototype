28d03a9f0bfb28605331e5dc5d538c18eccc240c
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-01-12 07:59:29 -0600

    Added TriggerEnemy Chest prefab

---
+++ Assets/Scripts/TriggerEnemyWave.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnemyWave : MonoBehaviour
+{
+    bool playerInRange;
+    public GameObject chestTrigger;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange)
+        {
+            
+            if (Input.GetButtonDown("Action"))
+            {
+                Destroy(chestTrigger);
+                gameManager.instance.updateWave(1);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+    
+    
+}
