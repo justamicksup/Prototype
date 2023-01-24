f248ef155cc35a9096bc21cb0dad10cc303d7317
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-01-24 17:50:48 -0600

    Folder Organization, Enemy Stat tweaks, colliders on melee and no weapon enemy to do damage

---
+++ Assets/Scripts/Enemy/Unused Ready for Delete/SpawnEnemy.cs
@@ -0,0 +1,20 @@
+// using System;
+// using UnityEngine;
+// using UnityEngine.AI;
+// using UnityEngine.Rendering.PostProcessing;
+//
+//
+// public class SpawnEnemy: MonoBehaviour
+//     {
+//         [SerializeField] private MasterEnemy masterEnemy;
+//         [SerializeField] private GameObject SpawnPoint;
+//         
+//         
+//         void spawnEnemy()
+//         {
+//             Instantiate(masterEnemy.Model, SpawnPoint.transform.position, Quaternion.identity);
+//                 
+//         }
+//         
+//
+//     }
---
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -0,0 +1,17 @@
+
+    using System;
+    using UnityEngine;
+
+    public class MeleeWeapon: MonoBehaviour
+    {
+        public int damage;
+
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                gameManager.instance.playerScript.takeDamage(damage);
+            }
+        }
+    }
