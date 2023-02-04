b1848ed28f30a1a2217f38878059fbdd954faa97
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-02-03 23:54:59 -0600

    Set up muzzles in prefabs set it using scriptable object function, bullets now instantiate for ranged weapons. Blunderbuss(shotgun) instantiates group of bullets that can deal separate damage . Made adjustments to sound and weapons stats

--- Assets/Scripts/Player/WeaponPickup.cs
+++ Assets/Scripts/Player/WeaponPickup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WeaponPickup : MonoBehaviour
 {
     [SerializeField] MasterWeapon weapon;
+   
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
