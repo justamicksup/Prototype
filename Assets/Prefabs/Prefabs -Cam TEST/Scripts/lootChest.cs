00ba2f7e371365f12f08f8f70274ff15ba9de52d
Syxyr <cdcarr@student.fullsail.edu>
2023-02-04 22:08:10 -0700

    Probably need this for the chests to work. Loot Chest Script

---
+++ Assets/Prefabs/Prefabs -Cam TEST/Scripts/lootChest.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lootChest : MonoBehaviour, actionObject
+{
+    [SerializeField] GameObject chest;
+    [Range(1, 1000)][SerializeField] int lootCost;
+    [SerializeField] Transform weaponTransform;
+    [SerializeField] MasterWeapon weapon;
+
+    [SerializeField] GameObject displayWeapon;
+    bool inRange;
+    bool canPurchase;
+
+    void Start()
+    {
+        displayWeapon.GetComponent<MeshFilter>().sharedMesh = weapon.Model.GetComponent<MeshFilter>().sharedMesh;
+        displayWeapon.GetComponent<MeshRenderer>().sharedMaterials = weapon.Model.GetComponent<MeshRenderer>().sharedMaterials;
+        displayWeapon.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Submit") && checkPlayerCoins())
+        {           
+            primaryAction();
+        }
+    }
+
+    public void primaryAction()
+    {
+        if(inRange)
+        {            
+            if(checkPlayerCoins()) 
+            {
+                gameManager.instance.playerScript.WeaponPickup(weapon);
+                gameManager.instance.playerScript.addCoins(-lootCost);
+            }
+        }
+    }
+
+    public void secondaryAction() 
+    { 
+        //None - maybe can be upgrade actions?
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            inRange = true;
+            displayWeapon.SetActive(true);
+            gameManager.instance.alertText.text = $"Weapon Cost: {lootCost}";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            inRange = false;
+            canPurchase = false;
+            gameManager.instance.alertText.text = "";
+            displayWeapon.SetActive(false);
+        }
+    }
+
+    private bool checkPlayerCoins()
+    {
+        if(gameManager.instance.playerScript.GetCoins() >= lootCost)
+        {
+            canPurchase = true;
+        }
+
+        return canPurchase;
+    }
+}
