8fcc33909777b2d936059a94784e541a7a4f49c2
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-02-08 08:51:49 -0600

    added areaSpawner script that didn;t migrate from last night, updated BossAi script that didnt migrate either. Places Coins and chest inside rooms and not floating underneath

---
+++ Assets/Scripts/AreaSpawners.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AreaSpawners : MonoBehaviour
+{
+    //[SerializeField] int enemiesToSpawn;
+    [SerializeField] float interval;
+    [SerializeField] private Wave wave;
+    [SerializeField] private BoxCollider area;
+    [SerializeField] float spawnStopDelay;
+    bool isSpawning;
+    bool playerInRange;
+   // int enemiesSpawned;
+    public float yMin = 0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange && !isSpawning)
+        {
+            StartCoroutine(spawn());
+        }
+
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(delay());
+        }
+    }
+
+    IEnumerator spawn()
+    {
+        var _enemy = wave.EnemiesInWave[Random.Range(0, wave.EnemiesInWave.Count - 1)];
+        isSpawning = true;
+        Vector3 areaMin = area.bounds.min;
+        Vector3 areaMax = area.bounds.max;
+        float x = Random.Range(areaMin.x, areaMax.x);
+        float z = Random.Range(areaMin.z, areaMax.z);
+        float y = area.transform.position.y;
+        Vector3 spawnPosition = new Vector3(x, y, z);
+        RaycastHit hit;
+        if (Physics.Raycast(spawnPosition, Vector3.down, out hit, Mathf.Infinity))
+        {
+            spawnPosition.y = Mathf.Max(yMin, hit.point.y);
+        }
+        Instantiate(_enemy, spawnPosition, Quaternion.identity);
+
+      //  enemiesSpawned++;
+        yield return new WaitForSeconds(interval);
+        isSpawning = false;
+    }
+
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(spawnStopDelay);
+        playerInRange = false;
+    }
+}
--- Assets/Scripts/Enemy/Enemy AI Scripts/BossAI.cs
+++ Assets/Scripts/Enemy/Enemy AI Scripts/BossAI.cs
@@ -9,46 +9,98 @@ public class BossAI : MonoBehaviour, IDamage
     [SerializeField] private int currentHealth;
     [SerializeField] private bool isInvincible = false;
     [SerializeField] public float transitionDuration = 2.0f;
-    public NavMeshAgent agent;
+    [SerializeField] int rotationSpeed;
+    public int attack;
+    
+    
+    [Header("----- Variables -----")]
+    Vector3 playerDir;
+    bool playerInRange; 
+    bool isShooting;
+    float angleToPlayer;
+    
+    [Header("----- Needed References -----")]
     public MasterEnemy masterEnemyScriptableObject;
-    bool playerInRange;
+    public Animator animator;
+    public NavMeshAgent agent;
+    public Transform headPos;
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Renderer model;
+    public AudioSource aud;
+    
+    [Header("----- Gun Stats -----")]
+    public int shootDamage = 5;
+    public int range = 5;
+    public float shootRate = 1f;
+    public float shootForce = 1;
+    public int bulletSpeed;
+    public int ammoCapacity = 6;
+    public int ammoRemaining = 6;
+    public float reloadTime = 0.1f;
+    public int shootAngle = 45;
+    public AudioClip[] audGunShot;
+    
+    
     
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        GetStats((BossScriptableObject)masterEnemyScriptableObject);
+        GetNavMesh();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+
+        agent.SetDestination(gameManager.instance.player.transform.position);
+       // animator.SetFloat("Speed", agent.velocity.normalized.magnitude);
+        
+       
+        
         if (!isInvincible)
         {
             float healthPercentage = (float)currentHealth / (float)maxHealth;
-
-            if (healthPercentage > 0.7f) 
-            {   //shooting
+        
+            if (healthPercentage > 0.7f)
+            {
+                if (playerInRange)
+                {
+                    CanSeePlayer();
+                }
+                
+                //shooting
                 //need shooting navmesh agent
                 // Perform attack sequence 1
             }
-            else if (healthPercentage > 0.4f) 
-            {   
-                StartCoroutine(TransitionToNextAttack());
+            else if (healthPercentage > 0.4f)
+            {
+                agent.stoppingDistance = 0;
+                GetComponent<SphereCollider>().radius = 3;
+                
+                if (playerInRange)
+                {
+                    gameManager.instance.playerScript.takeDamage(attack);
+                }
+                //StartCoroutine(TransitionToNextAttack());
                 //melee
                 //need melee navmesh agent
                 //Perform attack sequence 2
             }
             else if (healthPercentage > 0.2f) //throwing bombs
             {
-                StartCoroutine(TransitionToNextAttack());
+               // StartCoroutine(TransitionToNextAttack());
                 //explosives
                 //need explosives navmesh agent
                 //Perform attack sequence 3
             }
             else if (healthPercentage > 0.0f)
             {
-                StartCoroutine(TransitionToNextAttack());
+                //StartCoroutine(TransitionToNextAttack());
                 //explosives but faster
             }
             
@@ -123,6 +175,74 @@ public class BossAI : MonoBehaviour, IDamage
     
     void GetStats(BossScriptableObject _bossScriptableObject)
     {
+        attack = _bossScriptableObject.attack;
+        bulletSpeed = _bossScriptableObject.bulletSpeed;
         //store all the stats needed for all sequences
     }
+    
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        // if (agent.velocity.normalized.magnitude > 0)
+        // {
+        //     animator.SetTrigger("Shoot");
+        // }
+        // else
+        // {
+        //     animator.SetTrigger("IdleShoot");
+        // }
+
+        //animator.SetTrigger("Shoot");
+       // aud.PlayOneShot(audGunShot, audGunShotVol);
+        GameObject bulletClone = Instantiate(bullet, shootPos.position, bullet.transform.rotation);
+        bulletClone.GetComponent<Rigidbody>().velocity = (gameManager.instance.player.transform.position - headPos.transform.position).normalized * bulletSpeed;
+        bulletClone.GetComponent<bullet>().bulletDamage = shootDamage;
+        Debug.Log(bulletClone.GetComponent<bullet>().bulletDamage);
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+    
+    bool CanSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+        Debug.Log(angleToPlayer);
+
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    facePlayer();
+                }
+
+                if (!isShooting && angleToPlayer <= shootAngle)
+                {
+                    Debug.Log("Shooting player");
+                    StartCoroutine(shoot());
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    void facePlayer()
+    {
+        //don't rotate up or down (Y)
+        playerDir.y = 0;
+        //Quaternion for a rotation to player
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        //make rotation smooth with Lerp
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * rotationSpeed);
+    }
+    
 }
