efa1e44cef48357db5fb5006580fad36a023a5bf
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-02-08 13:35:33 -0600

    Set up BossAi to transition to different attacks based on health percentage. Right now after he dies, win menu pops up. Rough animation for boss as well

--- Assets/Scripts/Enemy/Enemy AI Scripts/BossAI.cs
+++ Assets/Scripts/Enemy/Enemy AI Scripts/BossAI.cs
@@ -5,51 +5,96 @@ using UnityEngine.AI;
 
 public class BossAI : MonoBehaviour, IDamage
 {
+    //Common
+    [SerializeField] int HP;
+    [SerializeField] int rotationSpeed;
+    [Range(10, 1000)] [SerializeField] int lootValue;
+    public DeathEffect deathEffect;
+    public int viewAngle;
     [SerializeField] private int maxHealth = 200;
     [SerializeField] private int currentHealth;
     [SerializeField] private bool isInvincible = false;
     [SerializeField] public float transitionDuration = 2.0f;
-    [SerializeField] int rotationSpeed;
-    public int attack;
-    
-    
-    [Header("----- Variables -----")]
-    Vector3 playerDir;
-    bool playerInRange; 
-    bool isShooting;
-    float angleToPlayer;
-    
+
+
+    //Gun
+    [Header("----- Projectile Enemy Stats From SO -----")]
+    public int shootDamage;
+
+    public int range;
+    public float shootRate;
+    public float shootForce;
+    public int ammoCapacity;
+    public int ammoRemaining;
+    public float reloadTime;
+    public AudioClip audGunShot;
+    public int shootAngle;
+    [SerializeField] int bulletSpeed;
+    public float stoppingDistOrig;
+
+
+    //Melee
+    [Header("----- Melee Enemy Stats From SO -----")] [SerializeField]
+    public int attack = 1;
+
+    public float swingRate = 0.1f;
+    public AudioClip audWeaponSwing;
+    public int swingAngle;
+
+
+    //Explosives
+    [Header("----- Explosive Enemy Stats From SO -----")] [SerializeField]
+    float throwingDistance = 10f;
+
+    [SerializeField] int forceForward = 1;
+    [SerializeField] int forceUpward = 1;
+    [SerializeField] int torque = 1;
+    [SerializeField] float coolDown = 3;
+    public int throwAngle;
+
+    [Header("----- Bomb Stats -----")] [SerializeField]
+    int timer = 1;
+
+    [SerializeField] int explosionRadius = 1;
+    [SerializeField] int explosionForce = 1;
+    [SerializeField] int explosiveDamage = 1;
+
+
+    //Drag references in
     [Header("----- Needed References -----")]
     public MasterEnemy masterEnemyScriptableObject;
+
     public Animator animator;
     public NavMeshAgent agent;
     public Transform headPos;
+    public Collider weaponCollider;
+    public MeleeWeapon weapon;
+    [SerializeField] Renderer[] model;
+    public AudioSource aud;
     [SerializeField] Transform shootPos;
     [SerializeField] GameObject bullet;
-    [SerializeField] Renderer model;
-    public AudioSource aud;
-    
-    [Header("----- Gun Stats -----")]
-    public int shootDamage = 5;
-    public int range = 5;
-    public float shootRate = 1f;
-    public float shootForce = 1;
-    public int bulletSpeed;
-    public int ammoCapacity = 6;
-    public int ammoRemaining = 6;
-    public float reloadTime = 0.1f;
-    public int shootAngle = 45;
-    public AudioClip[] audGunShot;
-    
-    
-    
+    public Transform throwingHand;
+    public GameObject bomb;
+
+
+    [Header("----- Variables -----")] private float healthPercentage;
+    Vector3 playerDir;
+    bool isSwinging;
+    bool isShooting;
+    bool isThrowing;
+    bool playerInRange;
+    float angleToPlayer;
+    bool hasWeapon;
+    [Range(0, 1)] [SerializeField] float audWeaponSwingtVol;
+    [Range(0, 1)] [SerializeField] float audGunShotVol;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
         GetStats((BossScriptableObject)masterEnemyScriptableObject);
         GetNavMesh();
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -58,157 +103,65 @@ public class BossAI : MonoBehaviour, IDamage
         playerDir = gameManager.instance.player.transform.position - headPos.position;
 
         agent.SetDestination(gameManager.instance.player.transform.position);
-       // animator.SetFloat("Speed", agent.velocity.normalized.magnitude);
-        
-       
-        
+        animator.SetFloat("Speed", agent.velocity.normalized.magnitude);
+        healthPercentage = (float)currentHealth / (float)maxHealth;
+
+
         if (!isInvincible)
         {
-            float healthPercentage = (float)currentHealth / (float)maxHealth;
-        
-            if (healthPercentage > 0.7f)
+            if (playerInRange)
             {
-                if (playerInRange)
-                {
-                    CanSeePlayer();
-                }
-                
-                //shooting
-                //need shooting navmesh agent
-                // Perform attack sequence 1
-            }
-            else if (healthPercentage > 0.4f)
-            {
-                agent.stoppingDistance = 0;
-                GetComponent<SphereCollider>().radius = 3;
-                
-                if (playerInRange)
-                {
-                    gameManager.instance.playerScript.takeDamage(attack);
-                }
-                //StartCoroutine(TransitionToNextAttack());
-                //melee
-                //need melee navmesh agent
-                //Perform attack sequence 2
-            }
-            else if (healthPercentage > 0.2f) //throwing bombs
-            {
-               // StartCoroutine(TransitionToNextAttack());
-                //explosives
-                //need explosives navmesh agent
-                //Perform attack sequence 3
-            }
-            else if (healthPercentage > 0.0f)
-            {
-                //StartCoroutine(TransitionToNextAttack());
-                //explosives but faster
-            }
-            
-            else
-            {
-                //stop all agent movement
-                //stop attacks
-                // The boss has died
-                // Perform death sequence
+                CanSeePlayer();
             }
         }
     }
 
-    public void takeDamage(int damage)
-    {
-        if (!isInvincible)
-        {
-            currentHealth -= damage;
-        }
-    }
     
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
-    }
+    #region SetStatsLogic
 
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
-        }
-    }
-    
-    
-    IEnumerator TransitionToNextAttack()
-    {
-        isInvincible = true;
-        // Perform transition sequence
-        yield return new WaitForSeconds(transitionDuration);
-        isInvincible = false;
-    }
-    
     void GetNavMesh()
     {
         // Overwrites attached NavMeshAgent with Scriptable Object NavMesh
         // Only variables not included 
         // Agent Type, Quality, Area Mask. (Set these in Nav Mesh Agent)
-        
+
         // OffSet
         agent.baseOffset = masterEnemyScriptableObject.navMesh.baseOffset;
-       
+
         // Steering
         agent.speed = masterEnemyScriptableObject.navMesh.speed;
         agent.angularSpeed = masterEnemyScriptableObject.navMesh.angularSpeed;
         agent.acceleration = masterEnemyScriptableObject.navMesh.acceleration;
         agent.stoppingDistance = masterEnemyScriptableObject.navMesh.stoppingDistance;
         agent.autoBraking = masterEnemyScriptableObject.navMesh.autoBraking;
-      
+
         // Obstacle Avoidance
         agent.radius = masterEnemyScriptableObject.navMesh.radius;
         agent.height = masterEnemyScriptableObject.navMesh.height;
         agent.avoidancePriority = masterEnemyScriptableObject.navMesh.AvoidancePriority;
-        
+
         // Path Finding
         agent.autoTraverseOffMeshLink = masterEnemyScriptableObject.navMesh.AutoTraverseOffMeshLink;
         agent.autoRepath = masterEnemyScriptableObject.navMesh.AutoRepath;
     }
-    
+
     void GetStats(BossScriptableObject _bossScriptableObject)
     {
         attack = _bossScriptableObject.attack;
         bulletSpeed = _bossScriptableObject.bulletSpeed;
+        maxHealth = _bossScriptableObject.health;
         //store all the stats needed for all sequences
     }
-    
-    IEnumerator shoot()
-    {
-        isShooting = true;
-        // if (agent.velocity.normalized.magnitude > 0)
-        // {
-        //     animator.SetTrigger("Shoot");
-        // }
-        // else
-        // {
-        //     animator.SetTrigger("IdleShoot");
-        // }
 
-        //animator.SetTrigger("Shoot");
-       // aud.PlayOneShot(audGunShot, audGunShotVol);
-        GameObject bulletClone = Instantiate(bullet, shootPos.position, bullet.transform.rotation);
-        bulletClone.GetComponent<Rigidbody>().velocity = (gameManager.instance.player.transform.position - headPos.transform.position).normalized * bulletSpeed;
-        bulletClone.GetComponent<bullet>().bulletDamage = shootDamage;
-        Debug.Log(bulletClone.GetComponent<bullet>().bulletDamage);
-
-        yield return new WaitForSeconds(shootRate);
-        isShooting = false;
-    }
+    #endregion
     
+
+    #region PlayerInteraction
+
     bool CanSeePlayer()
     {
         playerDir = gameManager.instance.player.transform.position - headPos.position;
         angleToPlayer = Vector3.Angle(playerDir, transform.forward);
-
-        Debug.Log(angleToPlayer);
 
 
         RaycastHit hit;
@@ -224,17 +177,61 @@ public class BossAI : MonoBehaviour, IDamage
                     facePlayer();
                 }
 
-                if (!isShooting && angleToPlayer <= shootAngle)
+                if (healthPercentage > 0.7f)
                 {
-                    Debug.Log("Shooting player");
-                    StartCoroutine(shoot());
+                    StartCoroutine(TransitionToNextAttack());
+                    animator.SetInteger("WeaponType", 2);
+                    if (!isShooting && angleToPlayer <= shootAngle)
+                    {
+                        StartCoroutine(shoot());
+                    }
                 }
+
+                else if (healthPercentage > 0.4f)
+                {
+                    animator.SetInteger("WeaponType", 1);
+                    agent.stoppingDistance = 2;
+                    GetComponent<SphereCollider>().radius = 3;
+
+                    if (!isSwinging)
+                    {
+                        Debug.Log("Melee");
+                        StartCoroutine(MeleeHit());
+                    }
+                }
+                else if (healthPercentage > 0.2f) //throwing bombs
+                {
+                    animator.SetInteger("WeaponType", 3);
+                    agent.stoppingDistance = 10;
+                    GetComponent<SphereCollider>().radius = 15;
+                    if (!isThrowing)
+                    {
+                        StartCoroutine(ThrowBomb());
+                    }
+                }
+                else if (healthPercentage > 0.0f)
+                {
+                    //explosives but faster
+                    animator.SetInteger("WeaponType", 3);
+                    coolDown = .1f;
+                    if (!isThrowing)
+                    {
+                        StartCoroutine(ThrowBomb());
+                    }
+                }
+                else
+                {
+                    Destroy(gameObject);
+                    gameManager.instance.youWin();
+                }
+
                 return true;
             }
         }
+
         return false;
     }
-    
+
     void facePlayer()
     {
         //don't rotate up or down (Y)
@@ -244,5 +241,130 @@ public class BossAI : MonoBehaviour, IDamage
         //make rotation smooth with Lerp
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * rotationSpeed);
     }
+
+    public IEnumerator flashDamage()
+    {
+        foreach (var mesh in model)
+        {
+            mesh.material.color = Color.red;
+        }
+
+        yield return new WaitForSeconds(0.15f);
+
+        foreach (var mesh in model)
+        {
+            mesh.material.color = Color.white;
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        if (!isInvincible)
+        {
+            weaponColliderOff();
+            currentHealth -= damage;
+            StartCoroutine(flashDamage());
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    IEnumerator TransitionToNextAttack()
+    {
+        isInvincible = true;
+        // Perform transition sequence
+        yield return new WaitForSeconds(transitionDuration);
+        isInvincible = false;
+    }
+
+    #endregion
     
+
+    #region GunLogic
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        // aud.PlayOneShot(audGunShot, audGunShotVol);
+        GameObject bulletClone = Instantiate(bullet, shootPos.position, bullet.transform.rotation);
+        bulletClone.GetComponent<Rigidbody>().velocity =
+            (gameManager.instance.player.transform.position - headPos.transform.position).normalized * bulletSpeed;
+        bulletClone.GetComponent<bullet>().bulletDamage = shootDamage;
+        Debug.Log(bulletClone.GetComponent<bullet>().bulletDamage);
+
+        yield return new WaitForSeconds(shootRate);
+
+        isShooting = false;
+    }
+
+    #endregion
+    
+
+    #region MeleeLogic
+
+    IEnumerator MeleeHit()
+    {
+        isSwinging = true;
+
+        animator.SetTrigger("MeleeHit");
+        yield return new WaitForSeconds(swingRate);
+
+        isSwinging = false;
+    }
+
+    public void weaponColliderOn()
+    {
+        weaponCollider.enabled = true;
+    }
+
+    public void weaponColliderOff()
+    {
+        weaponCollider.enabled = false;
+    }
+
+    #endregion
+    
+
+    #region ExplosivesLogic
+
+    IEnumerator ThrowBomb()
+    {
+        isThrowing = true;
+
+        animator.SetTrigger("Throw");
+        yield return new WaitForSeconds(coolDown);
+
+        isThrowing = false;
+    }
+
+    public void createBomb()
+    {
+        GameObject grenade = Instantiate(bomb, throwingHand.position, throwingHand.rotation);
+        Rigidbody grenadeRigidbody = grenade.GetComponent<Rigidbody>();
+        grenadeRigidbody.AddForce(transform.forward * forceForward + transform.up * forceUpward);
+        grenadeRigidbody.AddTorque(Random.insideUnitSphere * torque);
+
+        explosiveDamage = grenade.GetComponent<explosiveWeapon>().damage;
+        explosionRadius = grenade.GetComponent<explosiveWeapon>().range;
+        explosionForce = grenade.GetComponent<explosiveWeapon>().force;
+        timer = grenade.GetComponent<explosiveWeapon>().timer;
+    }
+
+    #endregion
 }
