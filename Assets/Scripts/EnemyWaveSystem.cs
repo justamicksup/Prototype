7d6c89de874a152da02c1901faf8fb79ab9d2ea2
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-02-13 09:20:34 -0600

    EnemyWaveSystem now handles array of boxColliders to use as random spawnPoints. Fixed issue with 1 extra enemy being spawned. Spawn Interval can be manually set or leave at 0 to spawn all at once in the random spots.

--- Assets/Scripts/EnemyWaveSystem.cs
+++ Assets/Scripts/EnemyWaveSystem.cs
@@ -10,34 +10,38 @@ using Random = UnityEngine.Random;
 
 public class EnemyWaveSystem : MonoBehaviour
 {
-    // public int enemiesInWave;
-    public Wave[] waves;
-    public GameObject miniBoss;
-    public GameObject boss;
-    public float waveDuration = 10.0f; // 2 minutes
-    public float coolDownDuration = 5.0f;
-    public float difficultyMultiplier = 1.0f;
-    public int currentWaveIndex = 0;
+   
+    [SerializeField] Wave[] waves;
+    [SerializeField] GameObject miniBoss;
+    [SerializeField] GameObject boss;
+    [SerializeField] float waveDuration = 10.0f; // 2 minutes
+    [SerializeField] float coolDownDuration = 5.0f;
+    [SerializeField] internal float difficultyMultiplier = 1.0f;
+    internal int currentWaveIndex = 0;
     private bool waveActive;
-    private int wallet;
+    
 
-    public float yMin = 0f;
-    [SerializeField] private BoxCollider area;
-    [SerializeField] private float timer;
+    private float yMin = 0f;
+    [SerializeField] private BoxCollider[] spawnLocations;
+    [SerializeField] private float spawnInterval;
     private bool isTriggerSet;
     private bool isSpawning;
-    [SerializeField] private int enemiesToSpawn;
-    [SerializeField] private int enemiesSpawned = 0;
-    public int currentWave = 0;
-    public  float waveDurationCountDown;
-    public float coolDownCountDown;
-    
-    
+    private int enemiesToSpawn;
+    private int enemiesSpawned = 1;
+   
+    private float waveDurationCountDown;
+    private float coolDownCountDown;
+
+
     private void Update()
     {
-        enemiesToSpawn = waves[currentWave].EnemyCount ;
+        enemiesToSpawn = waves[currentWaveIndex].EnemyCount;
+        if (enemiesSpawned < enemiesToSpawn)
+        {
+            waveActive = true;
+        }
 
-        if (isTriggerSet && !isSpawning && enemiesSpawned <= enemiesToSpawn)
+        if (isTriggerSet && !isSpawning && waveActive)
         {
             StartCoroutine(SpawnWaves());
         }
@@ -46,10 +50,10 @@ public class EnemyWaveSystem : MonoBehaviour
         if (waveActive && enemiesSpawned == enemiesToSpawn)
         {
             waveDurationCountDown -= Time.deltaTime;
-             waveDurationCountDown = Mathf.Max(waveDurationCountDown, 0);
+            waveDurationCountDown = Mathf.Max(waveDurationCountDown, 0);
             gameManager.instance.timer.text = waveDurationCountDown.ToString("0:00.00");
-
         }
+
         //Cool Down Timer
         if (!waveActive && isTriggerSet)
         {
@@ -58,12 +62,11 @@ public class EnemyWaveSystem : MonoBehaviour
             gameManager.instance.timer.text = coolDownCountDown.ToString("0:00.00");
         }
 
-        if (currentWave == waves.Length - 1)
+        if (currentWaveIndex == waves.Length - 1)
         {
             gameManager.instance.timer.text = "";
             Destroy(gameObject);
         }
-       
     }
 
     public void spawnTheWave()
@@ -75,86 +78,93 @@ public class EnemyWaveSystem : MonoBehaviour
     IEnumerator SpawnWaves()
     {
         isSpawning = true;
-        waveActive = true;
+        //waveActive = true;
 
-        Vector3 areaMin = area.bounds.min;
-        Vector3 areaMax = area.bounds.max;
-        float x = Random.Range(areaMin.x, areaMax.x);
-        float z = Random.Range(areaMin.z, areaMax.z);
-        float y = area.transform.position.y;
-        Vector3 spawnPosition = new Vector3(x, y, z);
-        RaycastHit hit;
-        if (Physics.Raycast(spawnPosition, Vector3.down, out hit, Mathf.Infinity))
+        if (spawnLocations != null)
         {
-            spawnPosition.y = Mathf.Max(yMin, hit.point.y);
-        }
+            BoxCollider box = spawnLocations[Random.Range(0, spawnLocations.Length)];
 
-        if (currentWave < waves.Length)
-        {
 
-            //reset clock
-            if (waveDurationCountDown <= 0)
+            Vector3 areaMin = box.bounds.min;
+            Vector3 areaMax = box.bounds.max;
+            float x = Random.Range(areaMin.x, areaMax.x);
+            float z = Random.Range(areaMin.z, areaMax.z);
+            float y = box.transform.position.y;
+            Vector3 spawnPosition = new Vector3(x, y, z);
+            RaycastHit hit;
+            if (Physics.Raycast(spawnPosition, Vector3.down, out hit, Mathf.Infinity))
             {
-                waveDurationCountDown = waveDuration;
+                spawnPosition.y = Mathf.Max(yMin, hit.point.y);
             }
 
-            // Check if it's the last wave
-            if (currentWave == waves.Length - 1)
+
+            if (currentWaveIndex < waves.Length)
             {
-                // Spawn the boss
-                Instantiate(boss, spawnPosition,
-                    Quaternion.LookRotation(gameManager.instance.player.transform.position));
-
-                // Wait for the boss to be defeated
-
-
-                // Boss defeated, call the ship to come
-
-            }
-            // Check if it's time for a mini boss wave
-            else if ((currentWave + 1) % 5 == 0)
-            {
-                // Spawn the mini boss
-                Instantiate(miniBoss, spawnPosition, Quaternion.identity);
-
-                // Wait for the mini boss to be defeated
-
-                // Mini boss defeated, continue with next wave
-            }
-            else
-            {
-                Instantiate(waves[currentWave].EnemiesInWave[Random.Range(0, waves[currentWave].EnemiesInWave.Count)],
-                    spawnPosition, Quaternion.LookRotation(gameManager.instance.player.transform.position));
-            }
-
-            if (enemiesSpawned == enemiesToSpawn)
-            {
-                // Wait for the wave duration
-                yield return new WaitForSeconds(waveDuration);
-
-                // Wave is over, set waveActive to false
-                waveActive = false;
-
                 //reset clock
-                if (coolDownCountDown <= 0)
+                if (waveDurationCountDown <= 0)
                 {
-                    coolDownCountDown = coolDownDuration;
+                    waveDurationCountDown = waveDuration;
                 }
 
-                // Wait for the cool down
-                yield return new WaitForSeconds(coolDownDuration);
-                enemiesSpawned = 0;
-                currentWave++;
-                gameManager.instance.updateWave();
-            }
-            else
-            {
-                enemiesSpawned++;
-                yield return new WaitForSeconds(timer);
-            }
+                // Check if it's the last wave
+                if (currentWaveIndex == waves.Length - 1)
+                {
+                    // Spawn the boss
+                    Instantiate(boss, spawnPosition,
+                        Quaternion.LookRotation(gameManager.instance.player.transform.position));
 
-            isSpawning = false;
+                    // Wait for the boss to be defeated
 
+
+                    // Boss defeated, call the ship to come
+                }
+                // Check if it's time for a mini boss wave
+                else if ((currentWaveIndex + 1) % 5 == 0)
+                {
+                    // Spawn the mini boss
+                    Instantiate(miniBoss, spawnPosition, Quaternion.identity);
+
+                    // Wait for the mini boss to be defeated
+
+                    // Mini boss defeated, continue with next wave
+                }
+                else
+                {
+                    Instantiate(
+                        waves[currentWaveIndex].EnemiesInWave[Random.Range(0, waves[currentWaveIndex].EnemiesInWave.Count)],
+                        spawnPosition, Quaternion.LookRotation(gameManager.instance.player.transform.position));
+                }
+
+                if (enemiesSpawned == enemiesToSpawn)
+                {
+                    // Wait for the wave duration
+                    yield return new WaitForSeconds(waveDuration);
+
+                    // Wave is over, set waveActive to false
+                    waveActive = false;
+
+                    //reset clock
+                    if (coolDownCountDown <= 0)
+                    {
+                        coolDownCountDown = coolDownDuration;
+                    }
+
+                    // Wait for the cool down
+                    yield return new WaitForSeconds(coolDownDuration);
+                    enemiesSpawned = 0;
+                    currentWaveIndex++;
+                    gameManager.instance.updateWave();
+                }
+                else
+                {
+                    enemiesSpawned++;
+                    yield return new WaitForSeconds(spawnInterval);
+                }
+
+            }
         }
+        isSpawning = false;
+
+        
     }
 }
