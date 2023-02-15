e2fd6366682d40114946115171ecb4fa0952e42b
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-02-15 15:18:05 -0600

    fixed warning in ProjectileEnemyAI

--- Assets/Scripts/Enemy/Enemy AI Scripts/ProjectileEnemyAI.cs
+++ Assets/Scripts/Enemy/Enemy AI Scripts/ProjectileEnemyAI.cs
@@ -2,233 +2,234 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ProjectileEnemyAI : MonoBehaviour, IDamage
+namespace Enemy.Enemy_AI_Scripts
 {
-    [Header("----- Projectile Enemy Stats From SO -----")] [SerializeField]
-    int HP;
-
-    [SerializeField] int rotationSpeed;
-    [Range(10, 1000)] [SerializeField] int lootValue;
-    public int shootDamage;
-    public int range;
-    public float shootRate;
-    public float shootForce;
-    public int ammoCapacity;
-    public int ammoRemaining;
-    public float reloadTime;
-    public AudioClip audGunShot;
-    public int shootAngle;
-    public int viewAngle;
-    [Range(15, 35)] [SerializeField] int bulletSpeed;
-    public DeathEffect deathEffect;
-
-    [Header("----- Needed References -----")]
-    public MasterEnemy masterEnemyScriptableObject;
-
-    public Animator animator;
-    public NavMeshAgent agent;
-    public Transform headPos;
-    [SerializeField] Transform shootPos;
-    [SerializeField] GameObject bullet;
-    [SerializeField] Renderer model;
-    public AudioSource aud;
-    [SerializeField] GameObject weaponDrop;
-    [SerializeField] private EnemyWaveSystem waveStats;
-
-    [Header("----- Variables -----")] Vector3 playerDir;
-    bool isShooting;
-    bool playerInRange;
-    float angleToPlayer;
-    public float stoppingDistOrig;
-    [Range(0, 1)] [SerializeField] float audGunShotVol;
-
-    [SerializeField] private Vector3 offSetPlayerDir = new Vector3(0, 1, 0);
-    // Start is called before the first frame update
-    void Start()
+    public class ProjectileEnemyAI : MonoBehaviour, IDamage
     {
-        gameManager.instance.updateEnemyRemaining(1);
-        GetStats((EnemyProjectileScriptableObjects)masterEnemyScriptableObject);
-        GetNavMesh();
-        stoppingDistOrig = agent.stoppingDistance;
-    }
+        [Header("----- Projectile Enemy Stats From SO -----")] [SerializeField]
+        int hp;
 
-    // Update is called once per frame
-    void Update()
-    {
-        playerDir = gameManager.instance.player.transform.position + offSetPlayerDir - headPos.position;
+        [SerializeField] int rotationSpeed;
+        [Range(10, 1000)] [SerializeField] int lootValue;
+        public int shootDamage;
+        public int range;
+        public float shootRate;
+        public float shootForce;
+        public int ammoCapacity;
+        public int ammoRemaining;
+        public float reloadTime;
+        public AudioClip audGunShot;
+        public int shootAngle;
+        public int viewAngle;
+        [Range(15, 35)] [SerializeField] int bulletSpeed;
+        public DeathEffect deathEffect;
+
+        [Header("----- Needed References -----")]
+        public MasterEnemy masterEnemyScriptableObject;
+
+        public Animator animator;
+        public NavMeshAgent agent;
+        public Transform headPos;
+        [SerializeField] Transform shootPos;
+        [SerializeField] GameObject bullet;
+        [SerializeField] Renderer model;
+        public AudioSource aud;
+        [SerializeField] GameObject weaponDrop;
+       // [SerializeField] private EnemyWaveSystem waveStats;
+
+        [Header("----- Variables -----")] Vector3 _playerDir;
+        bool _isShooting;
+        bool _playerInRange;
+        float _angleToPlayer;
+        public float stoppingDistOrig;
+        [Range(0, 1)] [SerializeField] float audGunShotVol;
+
+        [SerializeField] private Vector3 offSetPlayerDir = new Vector3(0, 1, 0);
+
+        private static readonly int Speed = Animator.StringToHash("Speed");
+        private static readonly int Shoot1 = Animator.StringToHash("Shoot");
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            gameManager.instance.updateEnemyRemaining(1);
+            GetStats((EnemyProjectileScriptableObjects)masterEnemyScriptableObject);
+            GetNavMesh();
+            stoppingDistOrig = agent.stoppingDistance;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var position = gameManager.instance.player.transform.position;
+            _playerDir = position + offSetPlayerDir - headPos.position;
         
-        agent.SetDestination(gameManager.instance.player.transform.position);
-        animator.SetFloat("Speed", agent.velocity.normalized.magnitude);
+            agent.SetDestination(position);
+            animator.SetFloat(Speed, agent.velocity.normalized.magnitude);
 
 
-        if (playerInRange)
-        {
-            CanSeePlayer();
-        }
-    }
-
-    // Need to adjust this in all AI to work like lecture
-    public void takeDamage(float damage)
-    {
-        HP -= (int)damage;
-        //animator.SetTrigger("Hit1");
-        StartCoroutine(flashDamage());
-        facePlayer();
-        agent.SetDestination(gameManager.instance.player.transform.position);
-        if (HP <= 0)
-        {
-            gameManager.instance.updateEnemyRemaining(-1);
-            gameManager.instance.playerScript.addCoins(lootValue);
-            deathEffect.DeathByEffects();
-
-            Destroy(gameObject);
-            gameManager.instance.DropLoot(transform, weaponDrop, true, true, true);
-        }
-    }
-
-    void facePlayer()
-    {
-        //don't rotate up or down (Y)
-        playerDir.y = 0;
-        //Quaternion for a rotation to player
-        Quaternion rot = Quaternion.LookRotation(playerDir);
-        //make rotation smooth with Lerp
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * rotationSpeed);
-    }
-
-    void GetStats(EnemyProjectileScriptableObjects _enemyProjectileScriptableObjects)
-    {
-        HP = (int)(_enemyProjectileScriptableObjects.health *
-                   gameManager.instance.enemyWaveSystem.difficultyMultiplier *
-                   (gameManager.instance.enemyWaveSystem.currentWaveIndex + 1));
-        shootDamage = (int)(_enemyProjectileScriptableObjects.shootDamage *
-                            gameManager.instance.enemyWaveSystem.difficultyMultiplier *
-                            (gameManager.instance.enemyWaveSystem.currentWaveIndex + 1));
-        range = _enemyProjectileScriptableObjects.range;
-        shootRate = _enemyProjectileScriptableObjects.shootRate;
-        shootForce = _enemyProjectileScriptableObjects.shootForce;
-        bulletSpeed = _enemyProjectileScriptableObjects.bulletSpeed;
-        ammoCapacity = _enemyProjectileScriptableObjects.ammoCapacity;
-        ammoRemaining = _enemyProjectileScriptableObjects.ammoRemaining;
-        reloadTime = _enemyProjectileScriptableObjects.reloadTime;
-        audGunShot =
-            _enemyProjectileScriptableObjects.audGunShot[
-                Random.Range(0, _enemyProjectileScriptableObjects.audGunShot.Length)];
-        shootAngle = _enemyProjectileScriptableObjects.shootAngle;
-        shootAngle = _enemyProjectileScriptableObjects.shootAngle;
-        viewAngle = _enemyProjectileScriptableObjects.viewAngle;
-        rotationSpeed = _enemyProjectileScriptableObjects.rotationSpeed;
-        deathEffect.SetDeathEffect(_enemyProjectileScriptableObjects.deathEffect);
-        // Haven't implemented defense yet
-        // Haven't implemented boss check yet
-    }
-
-    void GetNavMesh()
-    {
-        // Overwrites attached NavMeshAgent with Scriptable Object NavMesh
-        // Only variables not included 
-        // Agent Type, Quality, Area Mask. (Set these in Nav Mesh Agent)
-
-        // OffSet
-        agent.baseOffset = masterEnemyScriptableObject.navMesh.baseOffset;
-
-        // Steering
-        agent.speed = masterEnemyScriptableObject.navMesh.speed;
-        agent.angularSpeed = masterEnemyScriptableObject.navMesh.angularSpeed;
-        agent.acceleration = masterEnemyScriptableObject.navMesh.acceleration;
-        agent.stoppingDistance = masterEnemyScriptableObject.navMesh.stoppingDistance;
-        agent.autoBraking = masterEnemyScriptableObject.navMesh.autoBraking;
-
-        // Obstacle Avoidance
-        agent.radius = masterEnemyScriptableObject.navMesh.radius;
-        agent.height = masterEnemyScriptableObject.navMesh.height;
-        agent.avoidancePriority = masterEnemyScriptableObject.navMesh.AvoidancePriority;
-
-        // Path Finding
-        agent.autoTraverseOffMeshLink = masterEnemyScriptableObject.navMesh.AutoTraverseOffMeshLink;
-        agent.autoRepath = masterEnemyScriptableObject.navMesh.AutoRepath;
-    }
-
-    public IEnumerator flashDamage()
-    {
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.15f);
-        model.material.color = Color.white;
-    }
-
-    IEnumerator shoot()
-    {
-        isShooting = true;
-        // if (agent.velocity.normalized.magnitude > 0)
-        // {
-        //     animator.SetTrigger("Shoot");
-        // }
-        // else
-        // {
-        //     animator.SetTrigger("IdleShoot");
-        // }
-        animator.SetTrigger("Shoot");
-        aud.PlayOneShot(audGunShot, audGunShotVol);
-        GameObject bulletClone = Instantiate(bullet, shootPos.position, bullet.transform.rotation);
-        bulletClone.GetComponent<Rigidbody>().velocity =
-            (gameManager.instance.player.transform.position - headPos.transform.position).normalized * bulletSpeed;
-        bulletClone.GetComponent<bullet>().bulletDamage = shootDamage;
-        //Debug.Log(bulletClone.GetComponent<bullet>().bulletDamage);
-
-        yield return new WaitForSeconds(shootRate);
-        isShooting = false;
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
+            if (_playerInRange)
+            {
+                CanSeePlayer();
+            }
         }
 
-        Barricade b = other.GetComponent<Barricade>();
-        if (other.CompareTag("Destructible") && b != null &&  b.GetHP() > 0)
+        // Need to adjust this in all AI to work like lecture
+        public void takeDamage(float damage)
         {
-            StartCoroutine(shoot());
+            hp -= (int)damage;
+            //animator.SetTrigger("Hit1");
+            StartCoroutine(FlashDamage());
+            FacePlayer();
+            agent.SetDestination(gameManager.instance.player.transform.position);
+            if (hp <= 0)
+            {
+                gameManager.instance.updateEnemyRemaining(-1);
+                gameManager.instance.playerScript.addCoins(lootValue);
+                deathEffect.DeathByEffects();
+
+                Destroy(gameObject);
+                gameManager.instance.DropLoot(transform, weaponDrop, true, true, true);
+            }
         }
-    }
 
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        void FacePlayer()
         {
-            playerInRange = false;
+            //don't rotate up or down (Y)
+            _playerDir.y = 0;
+            //Quaternion for a rotation to player
+            Quaternion rot = Quaternion.LookRotation(_playerDir);
+            //make rotation smooth with Lerp
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * rotationSpeed);
         }
-    }
 
-    bool CanSeePlayer()
-    {
-        playerDir = gameManager.instance.player.transform.position + offSetPlayerDir - headPos.position;
-        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        void GetStats(EnemyProjectileScriptableObjects enemyProjectileScriptableObjects)
         {
+            hp = (int)(enemyProjectileScriptableObjects.health *
+                       gameManager.instance.enemyWaveSystem.difficultyMultiplier *
+                       (gameManager.instance.enemyWaveSystem.currentWaveIndex + 1));
+            shootDamage = (int)(enemyProjectileScriptableObjects.shootDamage *
+                                gameManager.instance.enemyWaveSystem.difficultyMultiplier *
+                                (gameManager.instance.enemyWaveSystem.currentWaveIndex + 1));
+            range = enemyProjectileScriptableObjects.range;
+            shootRate = enemyProjectileScriptableObjects.shootRate;
+            shootForce = enemyProjectileScriptableObjects.shootForce;
+            bulletSpeed = enemyProjectileScriptableObjects.bulletSpeed;
+            ammoCapacity = enemyProjectileScriptableObjects.ammoCapacity;
+            ammoRemaining = enemyProjectileScriptableObjects.ammoRemaining;
+            reloadTime = enemyProjectileScriptableObjects.reloadTime;
+            audGunShot =
+                enemyProjectileScriptableObjects.audGunShot[
+                    Random.Range(0, enemyProjectileScriptableObjects.audGunShot.Length)];
+            shootAngle = enemyProjectileScriptableObjects.shootAngle;
+            shootAngle = enemyProjectileScriptableObjects.shootAngle;
+            viewAngle = enemyProjectileScriptableObjects.viewAngle;
+            rotationSpeed = enemyProjectileScriptableObjects.rotationSpeed;
+            deathEffect.SetDeathEffect(enemyProjectileScriptableObjects.deathEffect);
+            // Haven't implemented defense yet
+            // Haven't implemented boss check yet
+        }
+
+        void GetNavMesh()
+        {
+            // Overwrites attached NavMeshAgent with Scriptable Object NavMesh
+            // Only variables not included 
+            // Agent Type, Quality, Area Mask. (Set these in Nav Mesh Agent)
+
+            // OffSet
+            agent.baseOffset = masterEnemyScriptableObject.navMesh.baseOffset;
+
+            // Steering
+            agent.speed = masterEnemyScriptableObject.navMesh.speed;
+            agent.angularSpeed = masterEnemyScriptableObject.navMesh.angularSpeed;
+            agent.acceleration = masterEnemyScriptableObject.navMesh.acceleration;
+            agent.stoppingDistance = masterEnemyScriptableObject.navMesh.stoppingDistance;
+            agent.autoBraking = masterEnemyScriptableObject.navMesh.autoBraking;
+
+            // Obstacle Avoidance
+            agent.radius = masterEnemyScriptableObject.navMesh.radius;
+            agent.height = masterEnemyScriptableObject.navMesh.height;
+            agent.avoidancePriority = masterEnemyScriptableObject.navMesh.AvoidancePriority;
+
+            // Path Finding
+            agent.autoTraverseOffMeshLink = masterEnemyScriptableObject.navMesh.AutoTraverseOffMeshLink;
+            agent.autoRepath = masterEnemyScriptableObject.navMesh.AutoRepath;
+        }
+
+        private IEnumerator FlashDamage()
+        {
+            var material = model.material;
+            material.color = Color.red;
+            yield return new WaitForSeconds(0.15f);
+            material.color = Color.white;
+        }
+
+        IEnumerator Shoot()
+        {
+            _isShooting = true;
+            // if (agent.velocity.normalized.magnitude > 0)
+            // {
+            //     animator.SetTrigger("Shoot");
+            // }
+            // else
+            // {
+            //     animator.SetTrigger("IdleShoot");
+            // }
+            animator.SetTrigger(Shoot1);
+            aud.PlayOneShot(audGunShot, audGunShotVol);
+            GameObject bulletClone = Instantiate(bullet, shootPos.position, bullet.transform.rotation);
+            bulletClone.GetComponent<Rigidbody>().velocity =
+                (gameManager.instance.player.transform.position - headPos.transform.position).normalized * bulletSpeed;
+            bulletClone.GetComponent<bullet>().bulletDamage = shootDamage;
+            //Debug.Log(bulletClone.GetComponent<bullet>().bulletDamage);
+
+            yield return new WaitForSeconds(shootRate);
+            _isShooting = false;
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _playerInRange = true;
+            }
+
+            Barricade b = other.GetComponent<Barricade>();
+            if (other.CompareTag("Destructible") && b != null &&  b.GetHP() > 0)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _playerInRange = false;
+            }
+        }
+
+        void CanSeePlayer()
+        {
+            _playerDir = gameManager.instance.player.transform.position + offSetPlayerDir - headPos.position;
+            _angleToPlayer = Vector3.Angle(_playerDir, transform.forward);
+
+            if (!Physics.Raycast(headPos.position, _playerDir, out var hit)) return;
             if (hit.collider.CompareTag("Player"))
             {
                 agent.SetDestination(gameManager.instance.player.transform.position);
 
                 if (agent.remainingDistance < agent.stoppingDistance)
                 {
-                    facePlayer();
+                    FacePlayer();
                 }
 
-                if (!isShooting && angleToPlayer <= shootAngle)
+                if (!_isShooting && _angleToPlayer <= shootAngle)
                 {
                     //Debug.Log("Shooting player");
-                    StartCoroutine(shoot());
+                    StartCoroutine(Shoot());
                 }
-
-                return true;
             }
         }
-
-        return false;
     }
 }
