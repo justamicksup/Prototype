3d2f02a81a0ff4cd01134e12be67743c0a61a181
justamicksup <102436319+justamicksup@users.noreply.github.com>
2023-02-15 16:09:43 -0600

    fixed warnings in cameraController

--- Assets/Scripts/Player/cameraController.cs
+++ Assets/Scripts/Player/cameraController.cs
@@ -1,51 +1,54 @@
 using UnityEngine;
 
-public class cameraController : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private Transform[] armTransform = new Transform[2];
-    //camera rotation limit
-    [SerializeField] int lockVerMin;
-    [SerializeField] int lockVerMax;
-
-    //inversion bool
-    [SerializeField] bool invertX;
-
-    float xRotation;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class cameraController : MonoBehaviour
     {
-        Cursor.visible= false;
-        Cursor.lockState= CursorLockMode.Locked;
-    }
+        [SerializeField] private Transform[] armTransform = new Transform[2];
+        //camera rotation limit
+        [SerializeField] int lockVerMin;
+        [SerializeField] int lockVerMax;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //get input
-        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * gameManager.instance.sensitivity;
-        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * gameManager.instance.sensitivity;
+        //inversion bool
+        [SerializeField] bool invertX;
 
-        if(invertX)
-            xRotation += mouseY;
-        else
-            xRotation -= mouseY;
+        float _xRotation;
 
-        //clamp camera rotation
-        xRotation = Mathf.Clamp(xRotation, lockVerMin, lockVerMax);
 
-        //rotate cameraX
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.visible= false;
+            Cursor.lockState= CursorLockMode.Locked;
+        }
 
-        //rotate playerY
-        transform.parent.Rotate(Vector3.up * mouseX);
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            //get input
+            float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * gameManager.instance.sensitivity;
+            float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * gameManager.instance.sensitivity;
 
-    private void LateUpdate()
-    {
-        //transform.localRotation = Quaternion.Euler(125, 0, 90);
-        armTransform[0].transform.localRotation = Quaternion.Euler(xRotation, armTransform[0].transform.localRotation.eulerAngles.y, armTransform[0].transform.localRotation.eulerAngles.z);
-        armTransform[1].transform.localRotation = Quaternion.Euler(-xRotation, armTransform[1].transform.localRotation.eulerAngles.y, armTransform[1].transform.localRotation.eulerAngles.z);
+            if(invertX)
+                _xRotation += mouseY;
+            else
+                _xRotation -= mouseY;
+
+            //clamp camera rotation
+            _xRotation = Mathf.Clamp(_xRotation, lockVerMin, lockVerMax);
+
+            //rotate cameraX
+            transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+
+            //rotate playerY
+            transform.parent.Rotate(Vector3.up * mouseX);
+        }
+
+        private void LateUpdate()
+        {
+            //transform.localRotation = Quaternion.Euler(125, 0, 90);
+            armTransform[0].transform.localRotation = Quaternion.Euler(_xRotation, armTransform[0].transform.localRotation.eulerAngles.y, armTransform[0].transform.localRotation.eulerAngles.z);
+            armTransform[1].transform.localRotation = Quaternion.Euler(-_xRotation, armTransform[1].transform.localRotation.eulerAngles.y, armTransform[1].transform.localRotation.eulerAngles.z);
+        }
     }
 }
